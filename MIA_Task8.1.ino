514bee89b1c11f7a0be0f43fc59bf89af367f6d1
basmala5essam <basmala20.5.2006@gmail.com>
2026-08-25 08:28:14 +0300

    Added main file with the 3 motor drivers' functions

--- MIA_Task8.1.ino
+++ MIA_Task8.1.ino
@@ -4,13 +4,38 @@
 
 
 void setup() {
-  L298N_init(int IN1, int IN2, int ENA);
-  Cytron_init(int DIR, int PWM);
-  BTS_init(int RPWM, int LPWM);
+  L298N_init(PA2, PA3, PA5);
+  Cytron_init(PA0 , PA1);
+  BTS_init(PA6, PA7);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //L268N movement demonstration
+  L298N_drive(PA2, PA3, PA5,200); //Forward with speed 200
+  delay(300);
+  L298N_drive(PA2, PA3, PA5,-150); //Backward with speed -150
+  delay(300);
+  L298N_drive(PA2, PA3, PA5,0);  //Stop
+  delay(1000);
+
+  //CytronMD10A movement demonstration
+
+
+  Cytron_drive(PA0 , PA1, 180);  //Forward with speed 180
+  delay(300);
+  Cytron_drive(PA0 , PA1, -60); //Backward with speed -60
+  delay(300);
+  Cytron_drive(PA0 , PA1, 0); //Stop
+  delay(1000);
+
+  //BTS7960 movement demonstration
+  BTS_drive(PA6, PA7,400);  //Forward with speed 255(max)
+  delay(300);
+  BTS_drive(PA6, PA7,-100);  //Backward with speed -100
+  delay(300);
+  BTS_drive(PA6, PA7,0); //Stop
+  delay(1000);
+
 
 }
